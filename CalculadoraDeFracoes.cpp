f66fc1cc413170573e4f7fb0e285b6fe327d7dff
luisfelipe207 <162385788+luisfelipe207@users.noreply.github.com>
2024-05-22 00:58:49 +0000

    a

--- CalculadoraDeFracoes.cpp
+++ CalculadoraDeFracoes.cpp
@@ -15,7 +15,7 @@ int mdc(int num2,  int num4) {
     
 
 int main() {
-   int  num1;int  num2;int  num3;int  num4;
+   int  num1;int  num2;int  num3;int  num4; int s;
     cout << "insira o primeiro numerador" << endl;
     cin >> num1;
     cout << "insira o primeiro denominador" << endl;
@@ -24,8 +24,25 @@ int main() {
     cin >> num3;
     cout << "insira o segundo denominador" << endl;
     cin >> num4;
+    cout << "escolha o sinal" << endl;
+    cin >> s; 
+
+    int soma = num2 + num4;
+    int subtacao = num2 - num4;
+    int multiplicacao = num2 * num4;
+    int divisao = num2 / num4;
+    int resto = num2 % num4;
 
     int  resultado;
     cout << "O MDC de " << num1 << " e " << num2 << " e: " << mdc ( num2,  num4) << endl;
+
+    if (soma = 1) {
+        int soma = num2 + num4;
+        cout << "o resultado é " << endl;
+        cin >> soma;
+
+    }
+
+
 return 0;
 }
